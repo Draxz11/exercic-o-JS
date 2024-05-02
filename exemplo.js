ff37160dd3b74664806af43cf28db4dc227cf6c8
MIGUEL SINHORETI CHIOVATTO <42255054809@info.local>
2024-05-02 17:26:16 -0300

    feat: Exercicio 1 feito

--- exemplo.js
+++ exemplo.js
@@ -1,17 +1,26 @@
-//Escreva um programa que solicite o nome do usuário e que lhe de boas vindas, exibindo uma mensagem pelo console.
+// Escreva um programa que solicite o nome do usuário e lhe dê boas vindas, 
+// exibindo uma mensagem pelo console.
 
-// prompt -> Recebe uma entrada digitada
-//let nome = prompt("Olá, digite seu nome:")
+// prompt() -> Recebe uma entrada digitada
+let nome = prompt("Olá, digite seu nome: ")
 
-//Concatenação
-//console.log("Seja bem vindo " + nome);
+// Concatenação
+console.log("Seja bem vindo " + nome + "!!!");
 
-//let pontos = 100
+let pontos = 100
 
-//let bonus = parseInt(prompt("Quantos pontos voce deseja somar?"))
+// parseInt() -> Transforma o que está entre () em número inteiro.
+let bonus =  parseInt( prompt("Quantos pontos você deseja somar?") )
 
-//console.log(pontos+bonus)
+// Adição -> num1 + num2
+// Subtração -> num1 - num2
+// Multiplicação -> num1 * num2
+// Divisão -> num1 / num2
 
-let notaAluno = parseFloat(prompt("Digite a nota do aluno"))
+let pontuacaoTotal = pontos + bonus
+console.log(pontuacaoTotal)
 
-console.log (notaAluno)
+// parseFloat() -> Transforma o que está entre () em número com , (quebrado)
+let notaAluno = parseFloat(prompt("Digite a nota do aluno: "))
+
+console.log(notaAluno);
