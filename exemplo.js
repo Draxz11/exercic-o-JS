b2d1ab473bfaadcf896c7c9475e4d1612f62ea6a
MIGUEL SINHORETI CHIOVATTO <42255054809@info.local>
2024-05-02 17:02:49 -0300

    feat:Exercicio JS

---
+++ exemplo.js
@@ -0,0 +1,17 @@
+//Escreva um programa que solicite o nome do usuário e que lhe de boas vindas, exibindo uma mensagem pelo console.
+
+// prompt -> Recebe uma entrada digitada
+//let nome = prompt("Olá, digite seu nome:")
+
+//Concatenação
+//console.log("Seja bem vindo " + nome);
+
+//let pontos = 100
+
+//let bonus = parseInt(prompt("Quantos pontos voce deseja somar?"))
+
+//console.log(pontos+bonus)
+
+let notaAluno = parseFloat(prompt("Digite a nota do aluno"))
+
+console.log (notaAluno)
