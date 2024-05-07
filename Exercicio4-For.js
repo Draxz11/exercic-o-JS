61296c792cedc7dd3841c76dcf362dbf6a8044a6
MIGUEL SINHORETI CHIOVATTO <42255054809@info.local>
2024-05-07 17:27:49 -0300

    exercicios for

---
+++ Exercicio4-For.js
@@ -0,0 +1,6 @@
+// 4. Faça um programa que imprima na tela a tabuada de um número
+// qualquer escolhido pelo usuário até o 10.
+
+for (let i = 0; i <= 90; i += 9) {
+    console.log(i)
+}
---
+++ Exercicio5-For.js
@@ -0,0 +1,10 @@
+// 5. Imprimir os primeiros 10 números da sequência de Fibonacci. (1, 1, 2, 3, 5, 8, 13, 21, 34, 55)
+
+for(i = 1; i <= 10; i++) {
+    console.log(c)
+
+    c = a + b
+    b = a
+    a = c
+
+ }
