614acc1f744431506787e065dccfaf92f7845e12
MIGUEL SINHORETI CHIOVATTO <42255054809@info.local>
2024-05-10 15:46:15 -0300

    feat: Correção dos Exercicios de FUnção e Repetição

--- ExercicioFuncao.js
+++ ExercicioFuncao.js
@@ -1,127 +1,57 @@
 // Fazer um menu de exercicios utilizando o exemplo dado em aula
-// Criar um menu, onde é possivel executar 6 dos exercícios anteriores.
-// Cada exercício deve ser encapsulado em uma função, e o menu deve executar essas funções.
-// O usuário podera escolher qual exercicio ele quer visualizar.
+// Criar um menu, onde é possível executar 6 dos exercícios anteriores.
+// Cada exercicio deve ser encapsulado em uma função, e o menu deve executar essas funções.
+// O usuário poderá escolher qual exercicio ele quer visualizar.
 
 // O usuário deverá digitar 0 para sair do menu.
 
-function imprimirImpar() {
-    for(let num = 0; num <= 50; num++){
-        if(num % 2 !== 0){
-            console.log(num);
-        }
-    }
-    
-    console.log("--------------------------------")
-    
-    for(let i = 1; i <= 50; i += 2){
-        console.log(i);
-    }
-}
-
-function imprimirPares() {
-    let num = 0;
-
-console.log("Números pares de 0 a 20: ");
-
-while(num <= 20){
-    if(num % 2 == 0){
-        console.log(num)
-    }
-    num ++;
-}
-}
-
-function idade() {
-    let idade = parseInt(prompt("Digite sua idade: "));
-
-while(idade < 18){
-    idade = parseInt(prompt("Você é menor de idade. Por favor, digite sua idade novamente: "))
-}
-
-console.log("Você é maior de idade. Sua idade é: " + idade);
-}
-
-function tabuada() {
-    let numero = parseInt(prompt("Digite um número para ver na tabuada: "))
-
-if(!isNaN(numero)){
-
-    for(let num = 1; num <=10; num++){
-        console.log(numero + " X " + num + " = " + (numero*num));
-    }
-
-}else{
-    console.log("Número invalido. Por favor, digite um número válido.")
-}
-
-
-console.log(isNaN("Óla")) // retorna true 
-console.log(isNaN(123)) // retorna false 
-
-//invertendo
-console.log(!isNaN("Óla")) // retorna false 
-console.log(!isNaN(123)) // retorna true 
-}
-
-
-function sequenciaFibonacci() {
-    let fib1 = 1;
-let fib2 = 1;
-
-console.log("Os primeiro 10 números da sequência de Fibonacci são: ")
-console.log(fib1);
-console.log(fib2);
-
-for(let i = 2; i < 10; i++){
-    let nextFib = fib1 + fib2;
-    console.log(nextFib);
-
-    fib1 = fib2;
-    fib2 = nextFib;
-}
-}
+// Hoisting                             - var e function
+// RECOMENDADO - Prevenir o hoisting    - let e const + arrow function
 
 function exemploRepeticao() {
-    let opcao = prompt("Digite uma opção\nDigite 0 para sair:")
+    let opcao = prompt("EXERCICIO DE REPETICAO\nDigite uma opção\nDigite 0 para sair:")
 
     while (opcao != "0") {
         console.log("Repetiuuuuuu");
-    
+
         opcao = prompt("R:Digite uma opção\nDigite 0 para sair:")
     }
-    
+
     console.log("Saiuuuuu");
 }
 
-let opcao = prompt("Escolha uma opção: \n1 - Exemplo Idade\n2 - Exemplo Repetição\n3 - Exemplo Sequência Fibonacci\n4 - Exemplo Tabuada\n5 - Exemplo Imprimir pares\n6 - Exemplo Imprimir Impares")
 
-switch (opcao) {
-    case "1":
-        idade()
-        break;
+const exercicioLogin = () => {
+    let username = prompt("Digite um nome de usuario:")
+    let senha = prompt("Digite a senha:")
 
-    case "2":
-        exemploRepeticao()
-        break;
 
-    case "3":
-        sequenciaFibonacci()
-        break;
-
-    case "4":
-        tabuada()
-        break;
-
-    case "5":
-        imprimirPares()
-        break;
-
-    case "6":
-        imprimirImpar()
-        break;
-
-        
-    default:
-    break;
+    if (username == "admin" && senha == "senha123") {
+        alert("Login bem sucedido");
+    } else {
+        alert("Voce não tem permissão de acesso!")
+    }
 }
+
+
+let opcaoUsuario;
+
+do {
+    opcaoUsuario = prompt("MENU\n\n1 - Exemplo de Repetição\n2 - Exercicio Usuario Senha\n\n0 - Digite 0 para sair")
+
+    if (opcaoUsuario == "0") {
+        alert("Voce escolheu sair :(")
+
+    } else if (opcaoUsuario == "1") {
+        // Mostrar exemplo de repeticao
+        exemploRepeticao()
+
+    } else if (opcaoUsuario == "2") {
+        // Mostrar exercicio usuario e senha
+        exercicioLogin()
+
+    } else {
+        alert("Exercicio inexistente")
+    }
+
+} while (opcaoUsuario != "0");
