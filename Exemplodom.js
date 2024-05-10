f5674a09465d68647e6ba95e340a8cfa99097617
MIGUEL SINHORETI CHIOVATTO <42255054809@info.local>
2024-05-10 17:23:51 -0300

    feat: ExemploDOM

---
+++ Exemplodom.js
@@ -0,0 +1,21 @@
+// Para textos dentro do body
+// document.write("Hello World")
+
+// Para Escrever direto do body
+document.body.innerHTML = "<h1>Teste H1</h1>"
+
+document.body.innerHTML += '<p id="texto">Outro teste</p>'
+
+let paragrafo = document.getElementById("texto")
+
+paragrafo.style.color = "blue"
+
+let opcao = window.prompt("Escolha a cor do paragrafo:\n1 - Verde\n2 - Vermelho")
+
+if (opcao == "1") {
+    paragrafo.style.color = "darkgreen"
+} else if (opcao == "2") {
+    paragrafo.style.color = "red"
+} else {
+    window.alert("Opcao invalida")
+}
